50c1bae38719d1b433f6a624b4dcea12e3711323
khoaTA1 <145079230+khoaTA1@users.noreply.github.com>
2025-09-13 19:55:29 +0700

    Add files via upload
    Tham khảo spring boot (kết nối cơ sở dữ liệu,...)

---
+++ src/main/java/khoa/repo/UserRepo.java
@@ -0,0 +1,13 @@
+package khoa.repo;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import khoa.entity.User;
+
+@Repository
+public interface UserRepo extends JpaRepository<User, Integer>{
+	List<User> findByUserNameContaining(String nuame);
+}
