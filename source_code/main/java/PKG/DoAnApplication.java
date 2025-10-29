b2f5f1e81c4816686e221bd2571fb4050636f67f
khoaTA1 <145079230+khoaTA1@users.noreply.github.com>
2025-10-29 17:31:49 +0700

    Add files via upload
    tải lên file Application.java

---
+++ source_code/main/java/PKG/DoAnApplication.java
@@ -0,0 +1,14 @@
+package PKG;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication(scanBasePackages = {"PKG.controller"})
+@ComponentScan
+public class DoAnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoAnApplication.class, args);
+	}
+}
